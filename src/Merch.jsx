e1fd37d24c4607e6ba521963f2e54d22921cf6c0
Drac Zihper <draczihper@gmail.com>
2025-03-02 22:53:09 +0300

    Remove tailwind class

--- src/Merch.jsx
+++ src/Merch.jsx
@@ -3,7 +3,7 @@ import merch from './assets/merch.jpg';
 function  Merch() {
     return <div>
         <p>This is an example of our merch</p>
-        <img src={merch} alt="" class="size-100" />
+        <img src={merch} alt="" />
 
     </div>
     
