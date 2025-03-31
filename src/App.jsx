734909a5cb8be8ba71826b36a84cbf7dadec15df
Drac Zihper <draczihper@gmail.com>
2025-03-31 19:55:08 +0300

    Render a list of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import './App.css'
 
+function ListItem(props){
+  return <li>{props.animal}</li>
+}
+
+function List(props){
+  return (
+    <ul>
+      {props.animals.map((animal) =>  {
+        return <ListItem key={animal} animal={animal} />
+      })}
+    </ul>
+  )
+}
+
+
 function App() {
   const animals = ['lion', 'cheetah', 'lynx', 'leopard', 'tiger']
   return (
     <div>
       <h1>Animal:</h1>
-      <ul>
-        {
-          animals.map((animal) => {
-            return <li key={animal}>{animal}</li>
-          })
-        }
-      </ul>
+      <List animals={animals} />
     </div>
   )
 }
