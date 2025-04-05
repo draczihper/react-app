d13f90b13206a94ff4d8893e19d802d679fd991d
Drac Zihper <draczihper@gmail.com>
2025-04-05 17:05:22 +0300

    Add a list to component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { people } from './data.js';
 import { getImageUrl } from './utils.js';
 
 export default function List() {
-  const chemists = people.filter(person => {
-    person.profession === 'chemist';
-
-    const everyoneElse = people.filter(person => {
+  const chemists = people.filter((person) =>
+    person.profession === 'chemist'
+);
+    const everyoneElse = people.filter(person => 
       person.profession !== 'chemist'
-    })
-  });
+    );
   return (
     <article>
       <h1>Scientists</h1>
@@ -22,9 +21,20 @@ export default function List() {
           <p>
           <b>{person.name}:</b>
             {" " + person.profession + " "} and is known for  {person.accomplishment}
-          
          </p>
           </li>
+      )}
+      </ul>
+        <h2>Everyone else</h2>
+      <ul>
+      {everyoneElse.map(person => 
+        <li key={person.id}>
+          <img src={getImageUrl(person)} alt={person.name} />
+          <p>
+            <b>{person.name}:</b>
+            {" " + person.profession + " "} and is known for {person.accomplishment}
+          </p>
+        </li>
       )}
       </ul>
     </article>
