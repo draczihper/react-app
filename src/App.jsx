605b2cff273d79fd5d62e4fb0993f8d3562ec4c9
Drac Zihper <draczihper@gmail.com>
2025-04-11 22:49:48 +0300

    Add gallery component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,57 @@
-function Button({text="Click me!", color = "blue", fontSize = 12, handleClick}) {
-  const buttonStyle = {
-    color: color,
-    fontSize: fontSize + "px"
-  };
+import { getImageUrl } from './utils.js';
 
-
-  return (
-    <button onClick={handleClick} style={buttonStyle}>
-      {text}
-      </button>
-  );
-}
-
-export default function App() {
-  const handleButtonClick = (url) => {
-    window.location.href = url
-  }
+export default function Gallery() {
   return (
     <div>
-      <Button  handleClick={() => handleButtonClick("https://www.google.com")}/>
+      <h1>Notable Scientists</h1>
+      <section className="profile">
+        <h2>Maria Skłodowska-Curie</h2>
+        <img
+          className="avatar"
+          src={getImageUrl('szV5sdG')}
+          alt="Maria Skłodowska-Curie"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b> 
+            physicist and chemist
+          </li>
+          <li>
+            <b>Awards: 4 </b> 
+            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
+          </li>
+          <li>
+            <b>Discovered: </b>
+            polonium (chemical element)
+          </li>
+        </ul>
+      </section>
+      <section className="profile">
+        <h2>Katsuko Saruhashi</h2>
+        <img
+          className="avatar"
+          src={getImageUrl('YfeOqp2')}
+          alt="Katsuko Saruhashi"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b> 
+            geochemist
+          </li>
+          <li>
+            <b>Awards: 2 </b> 
+            (Miyake Prize for geochemistry, Tanaka Prize)
+          </li>
+          <li>
+            <b>Discovered: </b>
+            a method for measuring carbon dioxide in seawater
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
