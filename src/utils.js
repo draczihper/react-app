f80a7bc6fd6aca012784321013836b5d97b000c8
Drac Zihper <draczihper@gmail.com>
2025-04-11 22:50:29 +0300

    Add a function to get url

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
-export function getImageUrl(person) {
-    return (
-      'https://i.imgur.com/' +
-      person.imageId +
-      's.jpg'
-    );
-  }
-  
+export function getImageUrl(imageId, size = 's') {
+  return (
+    'https://i.imgur.com/' +
+    imageId +
+    size +
+    '.jpg'
+  );
+}
