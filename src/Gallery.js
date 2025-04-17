c55e662103e857ee6f8a2925e5ed4591a4457c9a
Drac Zihper <draczihper@gmail.com>
2025-04-17 22:12:34 +0300

    Remove a state

--- src/Gallery.js
+++ src/Gallery.js
@@ -4,7 +4,6 @@ import { sculptureList } from './data.js';
 export default function Gallery() {
   const [index, setIndex] = useState(0);
   const [showMore, setShowMore] = useState(false);
-  const [showDetails, setShowDetails] = useState("details")
 
   function handleNextClick() {
     setIndex(index + 1);
