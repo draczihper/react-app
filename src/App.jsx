69fdca4b3ebb8771f0b7a687c04fcc39e0d4ddee
Drac Zihper <draczihper@gmail.com>
2025-04-21 22:23:20 +0300

    Add form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,34 @@
-import { useState } from 'react';
-import { sculptureList } from './data.js';
+export default function Form() {
+  let firstName = '';
+  let lastName = '';
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [showMore, setShowMore] = useState(false);
-
-  let hasPrev = index > 0;
-  let hasNext = index < sculptureList.length - 1;
-
-  function handlePrevClick() {
-    if(hasPrev) {
-      setIndex(index - 1);
-    }
+  function handleFirstNameChange(e) {
+    firstName = e.target.value;
   }
 
-  function handleNextClick() {
-    if (hasNext){
-      setIndex(index + 1);
-    }
+  function handleLastNameChange(e) {
+    lastName = e.target.value;
   }
 
-  function handleMoreClick() {
-    setShowMore(!showMore);
+  function handleReset() {
+    firstName = '';
+    lastName = '';
   }
 
-  let sculpture = sculptureList[index];
   return (
-    <>
-    <button onClick={handlePrevClick} disabled={!hasNext}>
-      Previous
-    </button>
-      <button onClick={handleNextClick} disabled={!hasNext}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i> 
-        by {sculpture.artist}
-      </h2>
-      <h3>  
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <button onClick={handleMoreClick}>
-        {showMore ? 'Hide' : 'Show'} details
-      </button>
-      {showMore && <p>{sculpture.description}</p>}
-      <img 
-        src={sculpture.url} 
-        alt={sculpture.alt}
+    <form onSubmit={e => e.preventDefault()}>
+      <input
+        placeholder="First name"
+        value={firstName}
+        onChange={handleFirstNameChange}
       />
-    </>
+      <input
+        placeholder="Last name"
+        value={lastName}
+        onChange={handleLastNameChange}
+      />
+      <h1>Hi, {firstName} {lastName}</h1>
+      <button onClick={handleReset}>Reset</button>
+    </form>
   );
 }
