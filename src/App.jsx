9fa13830f30b11584dc6d3b6d1e3560ef818d33c
Kelvin Mvungi <109474683+draczihper@users.noreply.github.com>
2025-04-23 14:21:15 +0000

    Remove state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
-
 export default function FeedbackForm() {
-  const [name, setName] = useState('');
 
   function handleClick() {
-    setName(prompt('What is your name?'));
+    let name = prompt('What is your name?');
     alert(`Hello, ${name}!`);
   }
 
