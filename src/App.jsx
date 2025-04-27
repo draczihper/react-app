74c298354162870f6b84c61c9defce8a68f6b3d3
Drac Zihper <draczihper@gmail.com>
2025-04-27 23:07:18 +0300

    Add form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
-function Component () {
-  const [count, setCount] = useState(0)
+import { useState } from 'react';
 
-  setCount(count + 1)
-
-  return <h1>{count}</h1>
+export default function Form() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState('Hi!');
+  if (isSent) {
+    return <h1>Your message is on its way!</h1>
+  }
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      setIsSent(true);
+      sendMessage(message);
+    }}>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
 }
 
-export default Component;
+function sendMessage(message) {
+  // ...
+}
