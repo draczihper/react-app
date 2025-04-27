a7677c53852f0add66efa413710f3bcfc2a9be03
Drac Zihper <draczihper@gmail.com>
2025-04-27 18:19:27 +0300

    Add recursive component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-function Person() {
-  const [person, setPerson] = useState({ name: "John", age: 100 });
+import { useState } from "react";
+function Component () {
+  const [count, setCount] = useState(0)
 
-  const handleIncreaseAge = () => {
-    console.log("in handleIncreaseAge (before setPerson call): ", person);
-    setPerson({ ...person, age: person.age + 1 });
-    // we've called setPerson, surely person has updated?
-    console.log("in handleIncreaseAge (after setPerson call): ", person);
-  };
+  setCount(count + 1)
 
-  // this console.log runs every time the component renders
-  // what do you think this will print?
-  console.log("during render: ", person);
-
-  return (
-    <>
-      <h1>{person.name}</h1>
-      <h2>{person.age}</h2>
-      <button onClick={handleIncreaseAge}>Increase age</button>
-    </>
-  );
+  return <h1>{count}</h1>
 }
 
-export default App;
+export default Component;
