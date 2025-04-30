0d2ab34163f1e0c19d7e7f91622807602d56dfa6
Drac Zihper <draczihper@gmail.com>
2025-04-30 22:26:16 +0300

    Add handleSubmit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react';
 
 export default function Form() {
-  const [isSent, setIsSent] = useState(false);
-  const [message, setMessage] = useState('Hi!');
-  if (isSent) {
-    return <h1>Your message is on its way!</h1>
+  const [to, setTo] = useState('Alice');
+  const [message, setMessage] = useState('Hello');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTimeout(() => {
+      alert(`You said ${message} to ${to}`);
+    }, 5000);
   }
+
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      setIsSent(true);
-      sendMessage(message);
-    }}>
+    <form onSubmit={handleSubmit}>
+      <label>
+        To:{' '}
+        <select
+          value={to}
+          onChange={e => setTo(e.target.value)}>
+          <option value="Alice">Alice</option>
+          <option value="Bob">Bob</option>
+        </select>
+      </label>
       <textarea
         placeholder="Message"
         value={message}
@@ -21,5 +31,3 @@ export default function Form() {
     </form>
   );
 }
-
-
