2f0feb4d0aa9996b6705a5903c7dfdf3378755c1
Drac Zihper <draczihper@gmail.com>
2025-05-19 20:10:54 +0300

    Import and render person component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";
-import Component from "./App.jsx";
+import Person from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Component />
+    <Person />
   </StrictMode>
 );
