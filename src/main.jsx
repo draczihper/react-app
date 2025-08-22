1296f34f4c5e0e09343debd767812618cd11fd6c
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2025-08-22 23:27:54 +0300

    Render form component

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ClassComponent from './components/ClassComponent.jsx'
+import ClassComponent from './components/ClassComponent.jsx';
+import Form from './components/FormComponent.jsx';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Form />
   </StrictMode>,
 )
