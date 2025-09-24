65dac030763989e14ef0f7b446791b7ed66c8fe9
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2025-09-24 22:40:54 +0300

    Add image component

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import Image from './components/ImageComponent'
+/* import './index.css'
 import App from './App.jsx'
 import ClassComponent from './components/ClassComponent.jsx';
-import Form from './components/FormComponent.jsx';
+import Form from './components/FormComponent.jsx'; */
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Form />
+    <Image/>
   </StrictMode>,
 )
