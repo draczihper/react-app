558f441a78d8602d124a0eb25e14335fb042d6b4
Drac Zihper <109474683+draczihper@users.noreply.github.com>
2025-10-28 10:52:27 +0300

    Test fetching data in react

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -1,25 +1,38 @@
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from "react";
 
-const Image = () => {
+const useImageURL = () => {
     const [imageURL, setImageURL] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/photos")
-            .then((response) => response.json())
+        fetch("https://jsonplaceholder.typicode.com/photos/", { mode: "cors" })
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error("server error")
+                }
+                return response
+            })
             .then((response) => setImageURL(response[0].url))
-            .catch((error) => console.error(error))
-    }, [])
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }, []);
 
+    return (imageURL, error, loading);
+};
 
-    return(
-        imageURL && (
-            <>
-            <h1>An Image</h1>
-            <img src={imageURL} alt={"placeholder text"} />
-            </>
-        )
+const Image = () => {
+    const {imageURL, error, loading} = useImageURL();
+
+    if(loading) return <p>Loading...</p>
+    if(error) return <p>An error has been encountered</p>
+
+    return (
+        <>
+        <h1>An image</h1>
+        <img src={imageURL} alt="placeholder text" />
+        </>
     )
 }
-
 
 export default Image;
